a4077af78e65c486aefce1b021bfca6d5c527a1b
Diana A <tdm2886@gmail.com>
2023-11-18 11:31:25 +0600

    add constants and utils

---
+++ product-service/src/constants.ts
@@ -0,0 +1,38 @@
+export const PRODUCTS = [
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae01",
+    title: "product1",
+    description: "description for product1",
+    price: 1500,
+  },
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae02",
+    title: "product2",
+    description: "description for product2",
+    price: 3000,
+  },
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae03",
+    title: "product3",
+    description: "description for product3",
+    price: 800,
+  },
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae04",
+    title: "product4",
+    description: "description for product4",
+    price: 1500,
+  },
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae05",
+    title: "product5",
+    description: "description for product5",
+    price: 3000,
+  },
+  {
+    id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae06",
+    title: "product6",
+    description: "description for product6",
+    price: 800,
+  },
+];
