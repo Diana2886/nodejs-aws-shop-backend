c3980926721481cef4e1dc985aae47003f6bc1b5
Diana A <tdm2886@gmail.com>
2023-11-26 10:58:58 +0600

    refactor

--- product-service/src/handlers/createProduct.ts
+++ product-service/src/handlers/createProduct.ts
@@ -5,14 +5,12 @@ import { v4 as uuidv4 } from "uuid";
 export const handler = async (event: any) => {
   try {
     console.log("event from createProduct", event);
-    
+
     const newProduct = {
       id: uuidv4(),
       ...JSON.parse(event.body),
     };
-
     await createProduct(newProduct);
-
     return buildResponse(200, newProduct);
   } catch (err: any) {
     if (err.errorName === "ValidationError") {
--- product-service/src/handlers/getProductsList.ts
+++ product-service/src/handlers/getProductsList.ts
@@ -5,10 +5,8 @@ import { Product } from "../types/product.interface";
 export const handler = async (event: any) => {
   try {
     console.log("event from getProductsList", event);
-    
-    const products: Product[] = await getProductsList();
-    console.log('PRODUCTS:', products)
 
+    const products: Product[] = await getProductsList();
     return buildResponse(200, products);
   } catch (err: any) {
     return buildResponse(500, {
