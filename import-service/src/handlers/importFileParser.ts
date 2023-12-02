4a0b858881c478102773e8cec811a4588fef6e07
Diana A <tdm2886@gmail.com>
2023-12-02 11:39:17 +0600

    add working readable stream

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -1,6 +1,13 @@
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  GetObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
 import csv from "csv-parser";
-import { Readable } from "stream";
+// import { Readable } from "stream";
+import { createWriteStream } from "node:fs";
+import { Readable } from "node:stream";
 
 export const handler = async (event: any) => {
   console.log("Event:", event);
@@ -22,20 +29,40 @@ export const handler = async (event: any) => {
 
     //   const s3Stream = await s3.getObject(params).createReadStream();
     const { Body } = await s3.send(new GetObjectCommand(params));
+    console.log("Start of the stream");
+    if (Body instanceof Readable) {
+      const stream = Readable.from(Body)
+        .pipe(csv())
+        .on("data", (data) => {
+          console.log("Parsed CSV record:", data);
+        })
+        .on("error", (error) => {
+          console.error("CSV parsing error:", error);
+        })
+        .on("end", async () => {
+          console.log("CSV parsing complete");
+          const inputForCopy = {
+            Bucket: bucket,
+            CopySource: `${bucket}/${event.Records[0].s3.object.key}`,
+            Key: event.Records[0].s3.object.key.replace("parsed", "uploaded"),
+          };
+          console.log("key", event.Records[0].s3.object.key);
+          const copyCommand = new CopyObjectCommand(inputForCopy);
+          const copyResponse = await s3.send(copyCommand);
+          console.log("copyResponse", copyResponse);
 
-    console.log('Start of the stream')
-    const stream = Readable.from([Body])
-      .pipe(csv())
-      .on("data", (data) => {
-        console.log("Parsed CSV record:", data);
-      })
-      .on("error", (error) => {
-        console.error("CSV parsing error:", error);
-      })
-      .on("end", () => {
-        console.log("CSV parsing complete");
-      });
-    console.log('stream:', stream)
+          const inputForDelete = {
+            Bucket: bucket,
+            Key: event.Records[0].s3.object.key,
+          };
+          const deleteCommand = new DeleteObjectCommand(inputForDelete);
+          const deleteResponse = await s3.send(deleteCommand);
+          console.log("deleteResponse", deleteResponse);
+          return {
+            statusCode: 202,
+          };
+        });
+    }
   } catch (error) {
     console.error("Error:", error);
   }
